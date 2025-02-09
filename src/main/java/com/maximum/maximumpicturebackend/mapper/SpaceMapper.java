8bcad06c63e5a8cdaef89e007ecf32640dd148c3
maximum <1291185069@qq.com>
2025-02-09 18:14:22 +0800

    feat: 空间管理

---
+++ src/main/java/com/maximum/maximumpicturebackend/mapper/SpaceMapper.java
@@ -0,0 +1,18 @@
+package com.maximum.maximumpicturebackend.mapper;
+
+import com.maximum.maximumpicturebackend.model.entity.Space;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author maximum
+* @description 针对表【space(空间)】的数据库操作Mapper
+* @createDate 2025-02-09 14:51:53
+* @Entity com.maximum.maximumpicturebackend.model.entity.Space
+*/
+public interface SpaceMapper extends BaseMapper<Space> {
+
+}
+
+
+
+
---
+++ src/main/java/com/maximum/maximumpicturebackend/service/SpaceService.java
@@ -0,0 +1,37 @@
+package com.maximum.maximumpicturebackend.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.maximum.maximumpicturebackend.model.dto.space.SpaceAddRequest;
+import com.maximum.maximumpicturebackend.model.dto.space.SpaceQueryRequest;
+import com.maximum.maximumpicturebackend.model.entity.Space;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.maximum.maximumpicturebackend.model.entity.User;
+import com.maximum.maximumpicturebackend.model.vo.space.SpaceVO;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+* @author maximum
+* @description 针对表【space(空间)】的数据库操作Service
+* @createDate 2025-02-09 14:51:53
+*/
+public interface SpaceService extends IService<Space> {
+    void validSpace(Space space, boolean add);
+
+    void fillSpaceBySpaceLevel(Space space);
+
+    QueryWrapper<Space> getQueryWrapper(SpaceQueryRequest spaceQueryRequest);
+
+    SpaceVO getSpaceVO(Space space, HttpServletRequest request);
+
+    Page<SpaceVO> getSpaceVOPage(Page<Space> spacePage, HttpServletRequest request);
+
+    /**
+     * 创建空间
+     * @param spaceAddRequest
+     * @param loginUser
+     * @return
+     */
+    long addSpace(SpaceAddRequest spaceAddRequest, User loginUser);
+}
